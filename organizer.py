7125f3abf80bafe6b0714f060852e2df29c7eab5
Johan John Sunny <johanjsunny@outlook.com>
2026-06-04 18:33:05 +0530

    running version made

--- organizer.py
+++ organizer.py
@@ -1,7 +1,8 @@
 from pathlib import Path 
+import shutil
 Category_Map = {
     "Images" : [".jpeg",".jpg",".gif",".png"],
-    "Documents" : [".pdf",".docx",".txt",".xlsx",".csv"],
+    "Documents" : [".pdf",".docx",".txt",".xlsx",".csv",".pptx"],
     "Audio" : [".mp3",".wav"],
     "Archives" : [".zip",".tar",".gz"]
 }
@@ -19,7 +20,17 @@ def organize_folder(target_directory):
             if file_extension in extensions:
                 target_category = category
                 break
+        
+        if target_category:
+            category_folder = base_dir/target_category
+            category_folder.mkdir(exist_ok=True)
+            destination_path = category_folder/item.name
 
+            try:
+                shutil.copy(str(item),str(destination_path))
+                print(f"Moved: {item.name} -> {target_category}/")
+            except Exception as e:
+                print(f"Error moving {item.name}: {e}")
 
 if __name__ == "__main__":
     organize_folder(input("Enter Folder path"))
