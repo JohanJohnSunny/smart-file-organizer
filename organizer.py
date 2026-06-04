73986441c21da213f376b37a6996d23185753cbf
Johan John Sunny <johanjsunny@outlook.com>
2026-06-04 18:13:34 +0530

    iteration through file folder

--- organizer.py
+++ organizer.py
@@ -1,4 +1,4 @@
-from pathlib import path 
+from pathlib import Path 
 Category_Map = {
     "Images" : [".jpeg",".jpg",".gif",".png"],
     "Documents" : [".pdf",".docx",".txt",".xlsx",".csv"],
@@ -7,7 +7,20 @@ Category_Map = {
 }
 
 def organize_folder(target_directory):
-    pass
+    base_dir = Path(target_directory)
+    if not base_dir.exists():
+        print("Error: File doesnt exists")
+        return
+    for item in base_dir.iterdir():
+        file_extension = item.suffix.lower()
+
+        target_category = None
+        for category, extensions in Category_Map.items():
+            if file_extension in extensions:
+                target_category = category
+                break
+
 
 if __name__ == "__main__":
     organize_folder(input("Enter Folder path"))
+
